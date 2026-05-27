48e7d9484cc2291b65cd8420f4b9aaee44350c47
imlaoyu <yu909105@gmail.com>
2026-05-27 18:39:14 +0800

    Add files via upload

---
+++ CompanySalary.java
@@ -0,0 +1,59 @@
+abstract class Employee {
+    public abstract double earnings();
+}
+
+class YearWorker extends Employee {
+    @Override
+    public double earnings() {
+        return 12000;
+    }
+}
+
+class MonthWorker extends Employee {
+    @Override
+    public double earnings() {
+        return 12 * 2300;
+    }
+}
+
+class WeekWorker extends Employee {
+    @Override
+    public double earnings() {
+        return 700 * 52;
+    }
+}
+
+class Company {
+    Employee[] employee;
+    double salaries = 0;
+
+    Company(Employee[] employee) {
+        this.employee = employee;
+    }
+
+    public double salariesPay() {
+        salaries = 0;
+        for (Employee value : employee) {
+            salaries += value.earnings();
+        }
+        return salaries;
+    }
+}
+
+public class CompanySalary {
+    public static void main(String[] args) {
+        Employee[] employee = new Employee[29];
+        for (int i = 0; i < employee.length; i++) {
+            if (i % 3 == 0) {
+                employee[i] = new WeekWorker();
+            } else if (i % 3 == 1) {
+                employee[i] = new MonthWorker();
+            } else if (i % 3 == 2) {
+                employee[i] = new YearWorker();
+            }
+        }
+
+        Company company = new Company(employee);
+        System.out.println("公司薪水总额:" + company.salariesPay() + "元");
+    }
+}

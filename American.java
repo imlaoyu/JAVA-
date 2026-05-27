8f8241925015816da3796366294fe9c8f131dcc0
imlaoyu <yu909105@gmail.com>
2026-05-27 18:36:50 +0800

    Add files via upload

---
+++ American.java
@@ -0,0 +1,16 @@
+public class American extends People{
+    public void speakHello(){
+        System.out.println("HOW DO U DO");
+    }
+    public void averageHeight(){
+        height=176;
+        System.out.println("American's average height:"+height+"cm");
+    }
+    public void averageWeight(){
+        weight=75;
+        System.out.println("American's average weight:"+weight+"kg");
+    }
+    public void americanBoxing(){
+        System.out.println("直拳，勾拳，组合拳");
+    }
+}
---
+++ Example.java
@@ -0,0 +1,24 @@
+public class Example {
+    public static void main(String[] args) {
+        Chinese chinaPeople=new Chinese();
+        American americanPeople=new American();
+        Beijingman beijingPeople=new Beijingman();
+
+        chinaPeople.speakHello();
+        americanPeople.speakHello();
+        beijingPeople.speakHello();
+
+        chinaPeople.averageHeight();
+        americanPeople.averageHeight();
+        beijingPeople.averageHeight();
+
+        chinaPeople.averageWeight();
+        americanPeople.averageWeight();
+        beijingPeople.averageWeight();
+
+        chinaPeople.chinaGongfu();
+        americanPeople.americanBoxing();
+        beijingPeople.chinaGongfu();
+    }
+}
+
